7adc4cabc0e7a67c41811238dc1762cf2cda1cc1
Victor Rayan <victor.rayan378@gmail.com>
2023-12-12 17:16:21 -0300

    feat: Add anemometer functionality and update soil moisture thresholds

--- main/sensor.cpp
+++ main/sensor.cpp
@@ -2,6 +2,7 @@
 #include "mqtt.h"
 #include "memoryFlash.h"
 #include "bomba.h"
+#include "sensor.h"
 
 #define DHTPIN 4
 #define DHTTYPE DHT22
@@ -17,12 +18,29 @@ int solo2 = 34;
 float umidade_solo1;
 float umidade_solo2;
 
-int solo_min1 = 1024;
-int solo_max1 = 3000;
+int solo_min1 = 300;
+int solo_max1 = 5100;
 int solo_min2 = 300;
 int solo_max2 = 5100;
 
 int i = 0, x = 0, f = 0;
+
+//variaveis anememotro
+const int anemometro = 19; // pino digital do anemômetro
+const int potenciometro = 15; // pino digital do potenciômetro
+// variáveis
+unsigned int pulsos = 0; // variável para contar os pulsos
+float direcao = 0;
+unsigned long tempo; // variável para registrar um tempo
+bool atual_estado = LOW;
+bool ultimo_estado = LOW; // variável para armazenar o estado do pino de interrupção
+unsigned int ultimo_debounce = 0;
+unsigned int tempo_debounce = 80;
+float pulsos_por_segundo = 0;
+float velocidade_do_vento = 0;
+const float multiplicador = 0.7989;
+const float defasagem = 0.28;
+
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -34,8 +52,12 @@ void setupSensor() {
   pinMode(temp_umi, INPUT);
   pinMode(solo1, INPUT);
   pinMode(solo2, INPUT);
-  
   dht.begin();
+
+  //setup anenometro
+  pinMode(anemometro, INPUT);
+  pinMode(potenciometro, INPUT);
+  attachInterrupt(digitalPinToInterrupt(anemometro), contaPulsos, RISING);
 }
 
 bool reservatorioVazio() {
@@ -44,22 +66,23 @@ bool reservatorioVazio() {
     
     Serial.print("Nível do reservatório: vazio");
     Serial.print('\n');
-    return true;
+    return false;
   }else{
     
     Serial.print("Nível do reservatório: cheio");
     Serial.print('\n');
-    return false;
+    return true;
   }
 }
 
 void verifySensor() {
   
-  reservatorio = reservatorioVazio();
-  Serial.print("Estado da bomba: ");
-  Serial.println(reservatorio);
-  if (reservatorio == true) {
-    closeBomba();
+  bool validade_reservatorio = reservatorioVazio();
+  Serial.print("Estado da reservatorio na leitura: ");
+  Serial.println(validade_reservatorio);
+  if (validade_reservatorio == false) {
+    Serial.print("Nível do reservatório deu vazio fechando bomba");
+   closeBomba();
   }
 
   umidadear = dht.readHumidity(); //Lê o nível de umidade do ar
@@ -85,10 +108,33 @@ void verifySensor() {
   Serial.print("Umidade do Solo 2 (percentual): ");
   Serial.print(umidade_solo2);
   Serial.print('\n');
-  float windSpeed = 5.0;
+
+  pulsos_por_segundo = (((float)pulsos/2)/((millis() - tempo)/5000.0)); // calcula pulsos por segundo e transforma para float
+  velocidade_do_vento = (multiplicador * pulsos_por_segundo) + defasagem;
+  float windSpeed = velocidade_do_vento * 3.6;
+
+  Serial.print("velocidade do vento ");
+  Serial.print(windSpeed);
+  Serial.print('\n');
+  
   bool status = loadBombaStatus();
+  Serial.print("medias Umidade do Solo (percentual): ");
+  float media_umidade = ((umidade_solo1 + umidade_solo2) / 2);
+  Serial.print(media_umidade);
+  Serial.print('\n');
 
-  sendMessageInfo(windSpeed, umidadear, umidade_solo1, temperatura, status, reservatorio);
+  sendMessageInfo(windSpeed, umidadear, media_umidade, temperatura, status, validade_reservatorio);
 
 
+}
+
+void contaPulsos() {
+
+  if ((millis() - ultimo_debounce) > tempo_debounce) {
+    atual_estado = digitalRead(anemometro); // Lê o estado do pino 19
+    //if (atual_estado == HIGH && ultimo_estado == LOW) {  // detecta borda de subida
+      ultimo_debounce = millis();
+      pulsos++;
+    //}
+  }
 }
